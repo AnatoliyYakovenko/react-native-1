f3d12872225ad658328c5f2bdb042f1c867a728e
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-06-02 11:27:32 +0300

    add fonts

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,14 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
-import { useFonts } from "expo-font";
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    "Inter-Black": require("./assets/fonts/Roboto-Bold.ttf"),
-  });
-  if (!fontsLoaded) {
-    return null;
-  }
+export default function LoginScreen() {
   return (
     <View style={styles.container}>
       <ImageBackground
