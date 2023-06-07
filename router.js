d9bf485c91cff06bd287bf80f8a55aaac8f8e843
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-06-07 15:07:03 +0300

    add icons and styles to bottom tab

--- router.js
+++ router.js
@@ -1,5 +1,9 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Pressable } from "react-native";
 
 import LoginScreen from "./Screens/LoginScreen";
 import RegistrationScreen from "./Screens/RegistrationScreen";
@@ -10,7 +14,7 @@ import CreatePostsScreen from "./Screens/CreatePostsScreen";
 const AuthStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
-export default function useRoute(isAuth){
+export default function useRoute(isAuth) {
   if (!isAuth) {
     return (
       <AuthStack.Navigator initialRouteName="Login">
@@ -28,10 +32,69 @@ export default function useRoute(isAuth){
     );
   }
   return (
-    <MainTab.Navigator>
-      <MainTab.Screen name="Posts" component={PostsScreen} />
-      <MainTab.Screen name="Create" component={CreatePostsScreen} />
-      <MainTab.Screen name="Profile" component={ProfileScreen} />
+    <MainTab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#FFFFFF",
+        tabBarShowLabel: false,
+        tabBarActiveBackgroundColor: "#FF6C00",
+        tabBarItemStyle: { borderRadius: 20, width: 70, height: 40 },
+        tabBarStyle: {
+          paddingTop: 9,
+          justifyContent: "center",
+          paddingLeft: 82,
+          paddingRight: 82,
+          paddingBottom: 47,
+        },
+        tabBarIconStyle: { color: "#212121" },
+      }}
+    >
+      <MainTab.Screen
+        options={{
+          headerShown: true,
+          title: "Публікації",
+          tabBarIcon: ({ focused, size, color }) => (
+            <SimpleLineIcons name="grid" size={24} color={color} />
+          ),
+        }}
+        name="Posts"
+        component={PostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          headerShown: true,
+          title: "Створити публікацію",
+          headerTitleStyle: {
+            fontSize: 17,
+            fontFamily: "Roboto-Medium",
+            color: "#212121",
+          },
+          headerStyle: {
+            borderBottomWidth: 0.3,
+            borderBottomColor: "#B3B3B3",
+          },
+          headerTitleAlign: "center",
+          headerLeft: ({ focused, size, color }) => (
+            <Pressable style={{ paddingLeft: 15 }}>
+              <AntDesign name="arrowleft" size={24} color="#212121" />
+            </Pressable>
+          ),
+          tabBarIcon: ({ focused, size, color }) => (
+            <AntDesign name="plus" size={24} color={color} />
+          ),
+        }}
+        name="Create"
+        component={CreatePostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => (
+            <Feather name="user" size={24} color={color} />
+          ),
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
     </MainTab.Navigator>
   );
-};
+}
