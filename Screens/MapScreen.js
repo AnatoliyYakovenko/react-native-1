707e290bfaf230a5b65dfa3259da9aff812b4e55
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-06-09 22:03:34 +0300

    HM5 complete

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -1,47 +1,65 @@
-import { View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function MapScreen({ route }) {
+  const coordinates = route.params.coordinates;
   const title = route.params.title;
   const location = route.params.location;
-  const coordinates = route.params.coordinates;
+  const [currentLocation, setCurrentLocation] = useState(null);
 
-  return coordinates.latitude && coordinates.longitude ? (
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      const coords = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      };
+      setCurrentLocation(coords);
+    })();
+  }, []);
+
+  return (
     <View style={styles.container}>
       <MapView
         style={styles.mapStyle}
-        region={{
-          ...coordinates,
-        }}
+        region={
+          coordinates.latitude && coordinates.longitude
+            ? coordinates
+            : currentLocation
+        }
         showsUserLocation={true}
       >
-        <Marker
-          title={title ? title : "Pic location"}
-          coordinate={coordinates}
-          location={location ? location : "Pic was taken here"}
-        />
+        {coordinates.latitude && coordinates.longitude ? (
+          <Marker
+            title={title ? title : "Pic location"}
+            coordinate={coordinates}
+            description={location ? location : "Pic was taken here"}
+          />
+        ) : (
+          currentLocation && (
+            <Marker
+              title="You are here"
+              coordinate={currentLocation}
+              description="Your current location"
+            />
+          )
+        )}
       </MapView>
     </View>
-  ) : (
-    <MapView
-      style={styles.mapStyle}
-      region={{
-        ...location,
-      }}
-      showsUserLocation={true}
-    >
-      {location && (
-        <Marker
-          title="You are here"
-          coordinate={location}
-          location="Your current location"
-        />
-      )}
-    </MapView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
